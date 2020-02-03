95f37658a49cfa412323fe65c3b1a728d2134eff
macaoyuan <macaoyuan@peiwo.cn>
2020-02-03 12:24:37 +0800

    fix: add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="icons">
+         <swiper>
+             <swiper-slide v-for="(page,index) of pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                        <img class="icon-img-content" :src="item.imgUrl" alt="">
+                    </div>
+                    <p class="icon-desc">{{item.desc}}</p>
+                </div>
+            </swiper-slide>
+         </swiper>
+    </div>
+</template>
+<script>
+  export default {
+    name: 'HomeIcons',
+    data() {
+      return {
+        iconList: [{
+          id: '001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        }, {
+          id: '002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '滑雪季节'
+        }, {
+          id: '003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '滑雪季节'
+        }, {
+          id: '004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '滑雪季节'
+        }, {
+          id: '005',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '滑雪季节'
+        }, {
+          id: '006',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '滑雪季节'
+        }, {
+          id: '007',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '滑雪季节'
+        }, {
+          id: '008',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '滑雪季节'
+        }, {
+          id: '009',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '滑雪季节'
+        }]
+      };
+    },
+    computed: {
+      pages() {
+        const pages = [];
+        this.iconList.forEach((item, index) => {
+          const page = Math.floor(index / 8);
+          if (!pages[page]) {
+            pages[page] = [];
+          }
+          pages[page].push(item);
+        });
+        return pages;
+      }
+    }
+  };
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl';
+  @import '~styles/mixins.styl';
+  .icons >>> .swiper-container
+    height: 0
+    padding-bottom: 50%
+  .icons
+    margin-top: .1rem
+    .icon
+      position relative
+      overflow hidden
+      width 25%
+      height 0
+      float left
+      padding-bottom 25%
+      .icon-img
+        position absolute
+        top 0px
+        left 0
+        rigth 0
+        bottom .44rem
+      .icon-img-content
+        display block
+        margin 0 auto
+        height 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+        ellipsis()
+</style>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -38,7 +38,7 @@
     width 100%
     height 0
     overflow hidden
-    padding-bottom 31.25%  //相对于宽度
+    padding-bottom 26.7%  //相对于宽度
     background #eee
     .swiper-img
         width 100%
